8e1cee617c4c4e6aa28a50ff6cec90cc1d46ff37
joshua campbell <joshua.campbell3@extendhealth.com>
2016-01-27 09:07:19 -0700

    added pseudocode for worker.sh

---
+++ controller.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+# get encrypted text
+
+# get lorem ipsum
+
+# divide keyspace per worker
+
+# pass execution to workers
+
+# wait for user to find correct key
+
+# write correct key to file
