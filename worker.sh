76cb54050c9600925972c9b9f2ff6f3e7ee6f0f2
joshua campbell <joshua.campbell3@extendhealth.com>
2016-01-27 09:08:51 -0700

    asdf

---
+++ worker.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+
+# get keyspace
+
+# get encrypted message
+
+# get lorem ipsum
+
+# generate a block of keys
+
+# encrypt lorem ipsum using keys
+
+# compare
+
+# repeat until out of keyspace
+
+# report back to controller
+
+
