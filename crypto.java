1ff6b60ac30971e522be861d9e8819dc5a49a1ff
joshua campbell <joshua.campbell3@extendhealth.com>
2016-02-01 09:24:44 -0700

    added crypto

---
+++ crypto.java
@@ -0,0 +1,35 @@
+package BIT430;
+
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import javax.crypto.Cipher;
+import javax.crypto.CipherInputStream;
+import javax.crypto.CipherOutputStream;
+import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.DESKeySpec;
+
+public class crypto {
+	public static void main(String[] args) {
+		Byte[] key = args[0];
+		String ptext = args[1];
+		String ctext = args[2];
+
+		while ( checkKey(key, ptext, ctext) ) {
+			
+		}
+	}
+
+	private static boolean checkKey (Byte[] key, String ptext, String ctext) {
+		
+		//encrypt ptext
+		DESKeySpec dks = new DESKeySPec(key);
+		SecretKeyFactory skf = SecretKeyFactory.getInstance("DES");
+		SecretKey desKey = skf.generateSecret("DES");
+
+		
+		//check against ctext
+		
+	}
+}
