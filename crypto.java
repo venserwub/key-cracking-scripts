67503952335f44f709583bfe011c03e2076306c8
joshua campbell <joshua.campbell3@extendhealth.com>
2016-02-01 11:29:16 -0700

    asdf

--- crypto.java
+++ crypto.java
@@ -1,4 +1,3 @@
-package BIT430;
 
 import java.io.InputStream;
 import java.io.OutputStream;
@@ -10,26 +9,46 @@ import javax.crypto.SecretKey;
 import javax.crypto.SecretKeyFactory;
 import javax.crypto.spec.DESKeySpec;
 
+import java.math.BigInteger;
+
 public class Crypto {
 	public static void main(String[] args) {
-		Byte[] key = args[0];
-		String ptext = args[1];
-		String ctext = args[2];
+		byte[] key = (new BigInteger(args[0], 2).toByteArray());
+		byte[] ptext;
+		byte[] ctext;
+		try {
+			ptext = args[1].getBytes("UTF8");
+			ctext = args[2].getBytes("UTF8");
+		}
+		catch (Exception ex) {
+		
+		}
 
-		while ( checkKey(key, ptext, ctext) ) {
-			
+		while ( !(checkKey(key, ptext, ctext)) ) {
+			key = (new BigInteger(key).add(BigInteger.valueOf(1))).toByteArray();
+			System.out.printf("0x%02X", key[0]);
+			System.out.println("");
 		}
 	}
 
-	private static boolean checkKey (Byte[] key, String ptext, String ctext) {
+	private static boolean checkKey (byte[] key, byte[] ptext, byte[] ctext) {
 		
 		//encrypt ptext
-		DESKeySpec dks = new DESKeySPec(key);
-		SecretKeyFactory skf = SecretKeyFactory.getInstance("DES");
-		SecretKey desKey = skf.generateSecret("DES");
+		try {
+			DESKeySpec dks = new DESKeySpec(key);
+			SecretKeyFactory skf = SecretKeyFactory.getInstance("DES");
+			SecretKey desKey = skf.generateSecret(dks);
+			Cipher cipher = Cipher.getInstance("DES");
+			cipher.init(Cipher.ENCRYPT_MODE, desKey);
 
+			if (cipher.doFinal(ptext) == ctext) {
+				return true;
+			}
+		}
+		catch (Exception ex) {
 		
-		//check against ctext
-		
+		}
+
+		return false;
 	}
 }
