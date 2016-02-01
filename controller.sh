0f149ceba239fd5c47ebad4b8ce043f93b3baeb3
Scott Crisenbery <scrisenbery@users.noreply.github.com>
2016-02-01 09:06:54 -0700

    Update controller.sh

--- controller.sh
+++ controller.sh
@@ -1,9 +1,10 @@
 #! /bin/bash
 
-# get encrypted text
 
-
-# get lorem ipsum
+javac CipherExample.java
+#Need to adjust it to pull the key from params if we generate a random one
+java CipherExample $RANDKEY
+#gets lorem ipsum and encrypts then decrypts
 
 
 # divide keyspace per worker
