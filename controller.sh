8ae87412e61bbb5c58d91efae29feb959b196779
Scott Crisenbery <scrisenbery@users.noreply.github.com>
2016-02-01 08:57:07 -0700

    Update controller.sh

--- controller.sh
+++ controller.sh
@@ -2,12 +2,18 @@
 
 # get encrypted text
 
+
 # get lorem ipsum
+
 
 # divide keyspace per worker
 
+
 # pass execution to workers
+
 
 # wait for user to find correct key
 
+
 # write correct key to file
+
