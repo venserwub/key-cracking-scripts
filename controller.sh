d369c1495aab84a82dbfd275f020d9a79a515d85
Scott Crisenbery <scrisenbery@users.noreply.github.com>
2016-02-01 10:58:51 -0700

    Update controller.sh

--- controller.sh
+++ controller.sh
@@ -1,21 +1,16 @@
 #! /bin/bash
 
-ssh [user]@[server] 'javac Crypto.java && nohup java Crypto $RANDKEY lorem.txt encrypted.txt > foo.out 2> foo.err < /dev/null &'
-
-ssh [user]@[server] 'javac Crypto.java && nohup java Crypto $RANDKEY lorem.txt encrypted.txt > foo.out 2> foo.err < /dev/null &'
-
-ssh [user]@[server] 'javac Crypto.java && nohup java Crypto $RANDKEY lorem.txt encrypted.txt > foo.out 2> foo.err < /dev/null &'
-
-ssh [user]@[server] 'javac Crypto.java && nohup java Crypto $RANDKEY lorem.txt encrypted.txt > foo.out 2> foo.err < /dev/null &'
-
-#gets lorem ipsum and encrypts then decrypts
-
-
 # divide keyspace per worker
 
 
 # pass execution to workers
+ssh [user]@[server] 'javac Crypto.java && nohup java Crypto $RANDKEY lorem.txt encrypted.txt > foo.out 2> foo.err < /dev/null &'
 
+ssh [user]@[server] 'javac Crypto.java && nohup java Crypto $RANDKEY lorem.txt encrypted.txt > foo.out 2> foo.err < /dev/null &'
+
+ssh [user]@[server] 'javac Crypto.java && nohup java Crypto $RANDKEY lorem.txt encrypted.txt > foo.out 2> foo.err < /dev/null &'
+
+ssh [user]@[server] 'javac Crypto.java && nohup java Crypto $RANDKEY lorem.txt encrypted.txt > foo.out 2> foo.err < /dev/null &'
 
 # wait for user to find correct key
 
